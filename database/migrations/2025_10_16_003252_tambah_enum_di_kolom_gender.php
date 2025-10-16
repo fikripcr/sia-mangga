dd57621707f7df325f90b268a96a299c698f9a2b
fikripcr <fikri@pcr.ac.id>
2025-10-16 11:19:11 +0700

    pertemuan 6

---
+++ database/migrations/2025_10_16_003252_tambah_enum_di_kolom_gender.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('pelanggan', function (Blueprint $table) {
+            // Mengubah tipe kolom 'gender' menjadi enum baru
+            $table->enum('gender', ['Pria', 'Wanita', 'Lainnya','Non-binary'])->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('pelanggan', function (Blueprint $table) {
+            $table->enum('gender', ['Male', 'Female', 'Other'])->nullable()->change();
+        });
+    }
+};
