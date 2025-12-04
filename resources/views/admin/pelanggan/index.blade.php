0c0dade0d828f225ab050876a392ee88fdfc11fa
fikripcr <fikri@pcr.ac.id>
2025-12-04 07:25:52 +0700

    pelanggan dummy

--- resources/views/admin/pelanggan/index.blade.php
+++ resources/views/admin/pelanggan/index.blade.php
@@ -35,6 +35,39 @@
             <div class="card border-0 shadow mb-4">
                 <div class="card-body">
                     <div class="table-responsive">
+                        <form method="GET" action="{{ route('pelanggan.index') }}" class="mb-3">
+                            <div class="row">
+                                <div class="col-md-2">
+                                    <select name="gender" class="form-select" onchange="this.form.submit()">
+                                        <option value="">All</option>
+                                        <option value="Male" {{ request('gender') == 'Male' ? 'selected' : '' }}>Male
+                                        </option>
+                                        <option value="Female" {{ request('gender') == 'Female' ? 'selected' : '' }}>Female
+                                        </option>
+                                    </select>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="input-group">
+                                        <input type="text" name="search" class="form-control" id="exampleInputIconRight"
+                                            value="{{ request('search') }}" placeholder="Search" aria-label="Search">
+                                        <button type="submit" class="input-group-text" id="basic-addon2">
+                                            <svg class="icon icon-xxs" fill="currentColor" viewBox="0 0 20 20"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path fill-rule="evenodd"
+                                                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                                    clip-rule="evenodd"></path>
+                                            </svg>
+                                        </button>
+                                        @if (request('search'))
+                                            <a href="{{ request()->fullUrlWithQuery(['search' => null]) }}"
+                                                class="btn btn-outline-secondary ml-3" id="clear-search"> Clear</a>
+                                        @endif
+                                    </div>
+                                </div>
+
+                            </div>
+                        </form>
+
                         <table id="table-pelanggan" class="table table-centered table-nowrap mb-0 rounded">
                             <thead class="thead-light">
                                 <tr>
@@ -68,14 +101,14 @@
                                                 </svg>
                                                 Edit
                                             </a>
-                                            <form action="{{route('pelanggan.destroy', $item->pelanggan_id)}}" method="POST" style="display:inline">
+                                            <form action="{{ route('pelanggan.destroy', $item->pelanggan_id) }}"
+                                                method="POST" style="display:inline">
                                                 @csrf
                                                 @method('DELETE')
                                                 <button type="submit" class="btn btn-danger btn-sm">
-                                                    <svg class="icon icon-xs me-2" data-slot="icon"
-                                                        fill="none" stroke-width="1.5" stroke="currentColor"
-                                                        viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
-                                                        aria-hidden="true">
+                                                    <svg class="icon icon-xs me-2" data-slot="icon" fill="none"
+                                                        stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24"
+                                                        xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                                         <path stroke-linecap="round" stroke-linejoin="round"
                                                             d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0">
                                                         </path>
@@ -89,6 +122,10 @@
 
                             </tbody>
                         </table>
+                        <div class="mt-3">
+                            {{ $dataPelanggan->links('pagination::bootstrap-5') }}
+                            {{-- {{ $dataPelanggan->links('pagination::simple-bootstrap-5') }} --}}
+                        </div>
                     </div>
                 </div>
             </div>
